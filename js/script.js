a8d20820b2d12bad42a849d31167ff37e16e4f69
Merajhosen <merajakhon7@gmail.com>
2022-02-17 22:52:10 +0000

    error handling and saving part

--- js/script.js
+++ js/script.js
@@ -11,7 +11,26 @@ document.getElementById('btn-calculate').addEventListener('click', function () {
     const rentCost = calculation('rent');
     const clothingCost = calculation('cloth');
 
+    const stringError = document.getElementById("string-error");
+    const negativeValueError = document.getElementById("negative-value-error");
 
+    // Error Handling for Negative Number and String
+    if (foodCost < 0 || rentCost < 0 || clothingCost < 0 || incomeAmount < 0) {
+        negativeValueError.style.display = "block";
+        stringError.style.display = "none";
+        return;
+    }
+
+    else if (
+        isNaN(foodCost) ||
+        isNaN(rentCost) ||
+        isNaN(clothingCost) ||
+        isNaN(incomeAmount)
+    ) {
+        stringError.style.display = "block";
+        negativeValueError.style.display = "none";
+        return;
+    }
 
     //  Total Expenses
     const totalExpenditure = foodCost + rentCost + clothingCost;
@@ -25,3 +44,32 @@ document.getElementById('btn-calculate').addEventListener('click', function () {
     const balance = document.getElementById("balance");
     balance.innerText = incomeAmount - totalExpenditure;
 })
+
+// savings part
+document
+    .getElementById("saving-percent")
+    .addEventListener("click", function () {
+        const balance = document.getElementById("balance");
+        const balanceAmount = parseFloat(balance.innerText);
+        const savingInput = document.getElementById("save");
+        const savingsPercentage = parseFloat(savingInput.value);
+        const errorMsg = document.getElementById("saving-error");
+        if (
+            savingsPercentage < 0 ||
+            isNaN(savingsPercentage) ||
+            balanceAmount <= 0
+        ) {
+            errorMsg.style.display = "block";
+            return;
+        }
+        document.getElementById("save").value = "";
+
+        const savingAmount = (balanceAmount * savingsPercentage) / 100;
+
+
+        document.getElementById("saved-amount").innerText = savingAmount;
+
+        const remainingBalance = balanceAmount - savingAmount;
+
+        document.getElementById("remaining-balance").innerText = remainingBalance;
+    });
